9fc3e18727572852f8502bdd4f9d9a0bc106f8a3
Alejandro Diaz <ixyz.dev@gmail.com>
2024-05-16 16:14:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,24 @@ import time
 vMotor = 15
 vMotorGiro = 7
 
-def sLinea ():
-    while (True):
-        # L1 Es TRUE si se esta viendo, caso contrario es FALSE
-        l1 = not cy.quad_rgb_sensor.is_line("L1", 1)
-        
-        # R1 Es TRUE si se esta viendo, caso contrario es FALSE
-        r1 = not cy.quad_rgb_sensor.is_line("R1", 1)
-        
-        # Avanzar hacia adelante
-        if l1 and r1: # L1 TRUE and R1 TRUE
-            m.drive_power(vMotor, -vMotor)
-           
-        # Girar a la izquierda
-        elif l1 and not r1: #L1 TRUE and R1 FALSE
-            m.drive_power(0, -vMotor)
-           
-        # Girar a la derecha
-        elif not l1 and r1: # L1 FALSE and R1 TRUE
-            m.drive_power(vMotor, 0)
-           
-        # No observa linea
-        else: # L1 FALSE and R1 FALSE
-            m.drive_power(0, 0)
+def sLinea():
+    # Obtiene la desviación del sensor
+    offset = cy.quad_rgb_sensor.get_offset_track(index=1)
+
+    # Define la velocidad base del motor
+    vMotor = 50  # Ajusta este valor según sea necesario
+
+    # Ajusta la velocidad de los motores basado en la desviación
+    if offset < -20:  # Desviación significativa a la izquierda
+        m.drive_power(0, -vMotor)
+    elif offset > 20:  # Desviación significativa a la derecha
+        m.drive_power(vMotor, 0)
+    else:  # Pequeña desviación o centrado
+        m.drive_power(vMotor, -vMotor)
+
+    # Detener si la desviación es extrema
+    if offset <= -100 or offset >= 100:
+        m.drive_power(0, 0)
 
 @cy.event.is_press("a")
 def main():

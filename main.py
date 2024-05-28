75e20382b0923b938cd30663c63a049eda2686a8
Alejandro Diaz <ixyz.dev@gmail.com>
2024-05-28 14:20:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,19 @@ import time
 vMotor = 15
 vMotorGiro = 7
 
-def sLinea():
-    # Obtiene la desviación del sensor
-    offset = cy.quad_rgb_sensor.get_offset_track(index=1)
+def line_follower():
+    """
+    Función que hace que el robot siga una línea.
+    """
+    offset = cy.quad_rgb_sensor.get_offset_track(index = 1)  # Obtenemos el offset
+    speed_adjustment = offset  # Ajustamos la velocidad basándonos en el offset
 
-    # Define la velocidad base del motor
-    vMotor = 50  # Ajusta este valor según sea necesario
+    # Configuramos la velocidad de los motores
+    motor_right_speed = -vMotor + ((speed_adjustment/2)*-1)  # Ajustamos la velocidad del motor derecho
+    motor_left_speed = vMotor + ((speed_adjustment/2)*-1)  # Ajustamos la velocidad del motor izquierdo
 
-    # Ajusta la velocidad de los motores basado en la desviación
-    if offset < -20:  # Desviación significativa a la izquierda
-        m.drive_power(0, -vMotor)
-    elif offset > 20:  # Desviación significativa a la derecha
-        m.drive_power(vMotor, 0)
-    else:  # Pequeña desviación o centrado
-        m.drive_power(vMotor, -vMotor)
-
-    # Detener si la desviación es extrema
-    if offset <= -100 or offset >= 100:
-        m.drive_power(0, 0)
+    # Controlamos los motores
+    m.drive_speed(motor_left_speed, motor_right_speed)
 
 @cy.event.is_press("a")
 def main():
